32bb32f9ffa286389786ad4a7075c92fe8114165
kalmer <kalmer@kalmer-GL752VW>
2020-07-25 04:02:19 +0300

    As I already included the configuration file, I don't want to remove it's reference, so I had to create it for the test server as well.

---
+++ settings/puud.tennis24.ee/Configuration.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace trees;
+
+class Configuration {
+	public const DB_HOST = 'localhost';
+	public const DB_NAME = 'trees';
+	public const DB_USER = 'trees';
+	public const DB_PASS = '85bbe3242761dc38';
+	public const BASE_FOLDER = '/projektid/';
+	public const CORE_FOLDER = Configuration::BASE_FOLDER . 'hard-core/';
+}
